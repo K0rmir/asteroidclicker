10a92c40235f2b4db863b7825b134015dca403b2
Edward Stewart <eddstewart1@hotmail.co.uk>
2023-12-06 21:21:59 +0000

    add javascript for first updade to increase score by 1 each second

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
 let score = document.getElementById("score");
 let counter = 0;
+
+
+// Click Asteroid function
 const asteroidBtn = document.getElementById("asteroidBtn");
 
-console.log(score.textContent)
-console.log(score.innerHTML)
-
-
-
-let clickAsteroid = () => {  
+const clickAsteroid = () => {  
     counter ++;
     score.textContent = counter;
 }
 
 asteroidBtn.addEventListener("click", clickAsteroid);
+
+// Increse Counter by 1 each second / Upgrade 1 ## SHIP MINER ##
+
+const shipMiner = document.getElementById("shipMiner");
+
+const upgrade1 = () => {
+    setInterval(function(){
+        counter ++;
+        score.textContent = counter;
+        console.log(counter);
+    }, 1000);
+}
+
+shipMiner.addEventListener("click", upgrade1);
+
+// 
+
+
+
 
 
 
