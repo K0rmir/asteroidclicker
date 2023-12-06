be8594fcedf005902eb4d007a4bba6a79428980d
Edward Stewart <eddstewart1@hotmail.co.uk>
2023-12-06 14:15:10 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+let rareMetals = document.getElementById("score");
+let score 
+
+
+
+
+const clickAsteroid = () => {
+    document.getElementById("counter");    
+    counter + 1;
+    score.textContent = counter;
+}
+
+
+
+
